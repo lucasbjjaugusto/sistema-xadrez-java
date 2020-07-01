0f4a79d0b483d4ead4994c21096c8697eb902c52
Lucas Augusto Santos de Oliveira <lucasbjjaugusto@gmail.com>
2020-07-01 17:54:19 -0300

    create class position and rename class program

---
+++ src/application/Program.java
@@ -0,0 +1,10 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+	public static void main(String[] args) {
+		Position pos = new Position(1, 2);
+		System.out.println(pos.toString());
+	}
+}
